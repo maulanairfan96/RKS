678ae1af498bcc98bcca800963ef6faa437e5236
IrfanMaulana98 <48825506+IrfanMaulana98@users.noreply.github.com>
2020-12-31 12:15:25 +0700

    Update

--- app/src/main/java/com/example/email/MainActivity.java
+++ app/src/main/java/com/example/email/MainActivity.java
@@ -16,6 +16,11 @@ public class MainActivity extends AppCompatActivity {
 
     public void create(View view) {
         Intent intent = new Intent(MainActivity.this, MainActivity2.class);
-                startActivity(intent);
+        startActivity(intent);
+    }
+
+    public void login(View view) {
+        Intent intent = new Intent(MainActivity.this, MainActivity3.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/email/MainActivity3.java
+++ app/src/main/java/com/example/email/MainActivity3.java
@@ -7,19 +7,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity2 extends AppCompatActivity {
+public class MainActivity3 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main3);
 
-        Button btnback = (Button) findViewById(R.id.button3);
+        Button btnback = (Button) findViewById(R.id.button5);
 
         btnback.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity2.this, MainActivity.class));
+                startActivity(new Intent(MainActivity3.this, MainActivity.class));
                 finish();
             }
         });
